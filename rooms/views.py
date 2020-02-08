f32dc61b76036fe1a191f126493783dafaafe57a
wansook <wansook0316@gmail.com>
2020-02-08 17:00:18 +0900

    #11.1 Pagination with Limit and Offset

--- rooms/views.py
+++ rooms/views.py
@@ -4,6 +4,10 @@ from . import models
 
 
 def all_rooms(request):
-    all_rooms = models.Room.objects.all()
+    page = int(request.GET.get("page", str(1)))  # default 값이 두번쨰 인자
+    page_size = 10
+    limit = page_size * page
+    offset = limit - page_size
+    all_rooms = models.Room.objects.all()[offset:limit]
 
     return render(request, "rooms/home.html", context={"rooms": all_rooms})
