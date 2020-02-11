2a2c911d17829d8375293bbd784eb0c2388b9cfb
wansook <wansook0316@gmail.com>
2020-02-11 16:47:09 +0900

    #11.5 get_page vs page

--- rooms/views.py
+++ rooms/views.py
@@ -6,8 +6,11 @@ from . import models
 
 
 def all_rooms(request):
-    page = request.GET.get("page")  # default 값이 두번쨰 인자
+    page = request.GET.get("page", 1)  # default 값이 두번쨰 인자
     room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10)  # 두번쨰 인지 페이지 수)
-    rooms = paginator.get_page(page)
-    return render(request, "rooms/home.html", {"rooms": rooms})
+    paginator = Paginator(
+        room_list, 10, orphans=5
+    )  # 두번쨰 인지 페이지 안의 행 개수, orphan은 마지막에 고아로 남는 행들을 마지막에 몇개가 생기면 보여줄 것인지
+    # rooms = paginator.get_page(page)
+    rooms = paginator.page(int(page))
+    return render(request, "rooms/home.html", {"page": rooms})
