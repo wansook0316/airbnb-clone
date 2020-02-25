0832b66972a163846f6418193a187db0b6d68fd0
wansook <wansook0316@gmail.com>
2020-02-25 21:26:24 +0900

    #11.8 Class Based Views part Two

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,6 @@
 from django.utils import timezone
 from django.views.generic import ListView
+from django.shortcuts import render
 from . import models
 
 
@@ -22,4 +23,8 @@ class HomeView(ListView):
         now = timezone.now()
         context["now"] = now
         return context
+
+
+def room_detail(request):
+    render(request,)
 
